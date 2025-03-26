1404438e8d98ea78ffdd73049494835bf56ba5e1
nicnonnious <nicpouger@gmail.com>
2025-03-25 19:44:30 -0500

    update gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,8 @@
 workers = 4
 bind = "0.0.0.0:10000"
 timeout = 120
+worker_class = "sync"
+forwarded_allow_ips = '*'
+secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
 accesslog = "-"
 errorlog = "-" 
